20f6e911f04606e7c99479da7c28647a0e175269
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-18 18:26:23 -0600

    cambios

--- resources/views/pages/rtl.blade.php
+++ resources/views/pages/rtl.blade.php
@@ -10,15 +10,15 @@
                 <div class="col-12">
                     <div class="card my-4">
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                            <div class="mb-3">
+                            <div class="mb-1">
                                 <form action="{{ route('libros.search') }}" method="GET">
-                                    <div class="mb-3">
+                                    
                                         <label for="buscar" class="form-label">Buscar por título</label>
                                         <div class="input-group">
                                             <input type="text" class="form-control-lg" id="buscar" name="buscar" placeholder="Ingresa el título del libro...">
                                             <button type="submit" class="btn btn-primary">Buscar</button>
                                         </div>
-                                    </div>
+                                    
                                 </form>
                                 <!-- BUSCADOR POR LETRA -->
                                 <label for="letra" class="form-label">Buscar por letra</label>
