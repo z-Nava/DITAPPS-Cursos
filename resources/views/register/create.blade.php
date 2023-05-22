8e36f1e1ea72ed397c8b76579953892874219ab5
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-22 10:49:03 -0600

    cambios

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -15,14 +15,68 @@
                 <div class="page-header min-vh-100">
                     <div class="container">
                         <div class="row">
-                            <div
-                                class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
-                                <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center"
-                                    style="background-image: url('../assets/img/illustrations/illustration-signup.jpg'); background-size: cover;">
-                                </div>
+
+                            <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-4 start-0 text-center justify-content-center flex-column">
+                                <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                    <div class="carousel-inner mb-4">
+                                      <div class="carousel-item">
+                                        <div class="page-header min-vh-75 m-3 border-radius-xl" style="background-image: url('https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/products/product-3-min.jpg');">
+                                          <span class="mask bg-gradient-dark"></span>
+                                          <div class="container">
+                                            <div class="row">
+                                              <div class="col-lg-6 my-auto">
+                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">Pricing Plans</h4>
+                                                <h1 class="text-white fadeIn2 fadeInBottom">Work with the rockets</h1>
+                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">Wealth creation is an evolutionarily recent positive-sum game. Status is an old zero-sum game. Those attacking wealth creation are often just seeking status.</p>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                       <div class="carousel-item">
+                                        <div class="page-header min-vh-75 m-3 border-radius-xl" style="background-image: url('https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/products/product-1-min.jpg');">
+                                          <span class="mask bg-gradient-dark"></span>
+                                          <div class="container">
+                                            <div class="row">
+                                              <div class="col-lg-6 my-auto">
+                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">Our Team</h4>
+                                                <h1 class="text-white fadeIn2 fadeInBottom">Work with the best</h1>
+                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">Free people make free choices. Free choices mean you get unequal outcomes. You can have freedom, or you can have equal outcomes. You can’t have both.</p>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                      <div class="carousel-item active">
+                                        <div class="page-header min-vh-75 m-3 border-radius-xl" style="background-image: url('https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/products/product-2-min.jpg');">
+                                          <span class="mask bg-gradient-dark"></span>
+                                          <div class="container">
+                                            <div class="row">
+                                              <div class="col-lg-6 my-auto">
+                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">Office Places</h4>
+                                                <h1 class="text-white fadeIn2 fadeInBottom">Work from home</h1>
+                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">You’re spending time to save money when you should be spending money to save time.</p>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div class="min-vh-75 position-absolute w-100 top-0">
+                                      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-bs-slide="prev">
+                                        <span class="carousel-control-prev-icon position-absolute bottom-50" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Previous</span>
+                                      </a>
+                                      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next">
+                                        <span class="carousel-control-next-icon position-absolute bottom-50" aria-hidden="true"></span>
+                                        <span class="visually-hidden">Next</span>
+                                      </a>
+                                    </div>
+                                  </div>
                             </div>
-                            <div
-                                class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
+
+
+                            <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
                                 <div class="card card-plain">
                                     <div class="card-header">
                                         <h4 class="font-weight-bolder">Registro</h4>
