0d77973765e955c5523e20210b6174285f85ef69
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-22 09:42:18 -0600

    aaaa

--- resources/views/pages/tables.blade.php
+++ resources/views/pages/tables.blade.php
@@ -91,15 +91,6 @@
                     <button type="submit" class="btn btn-primary">Guardar</button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
                   </div>
-                  @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
                 </form>
               </div>
             </div>
