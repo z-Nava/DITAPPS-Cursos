1a47f20cc17be615cb96abc35430bec406fd415e
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-21 23:10:36 -0600

    Modificacion

--- database/migrations/2023_05_22_035352_create_alumnos_table.php
+++ database/migrations/2023_05_22_035352_create_alumnos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('alumnos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('curso_id')->constrained('cursos');
             $table->timestamps();
         });
     }
