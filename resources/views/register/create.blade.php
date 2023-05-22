e4f0218bcb4c68f677197f5f82240177260bcfad
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-22 11:12:12 -0600

    carrousel

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -23,11 +23,11 @@
                                         <div class="page-header min-vh-75 m-3 border-radius-xl" style="background-image: url('https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/products/product-3-min.jpg');">
                                           <span class="mask bg-gradient-dark"></span>
                                           <div class="container">
-                                            <div class="row">
+                                            <div class="row justify-content-center">
                                               <div class="col-lg-6 my-auto">
-                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">Pricing Plans</h4>
-                                                <h1 class="text-white fadeIn2 fadeInBottom">Work with the rockets</h1>
-                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">Wealth creation is an evolutionarily recent positive-sum game. Status is an old zero-sum game. Those attacking wealth creation are often just seeking status.</p>
+                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">CURSO</h4>
+                                                <h1 class="text-white fadeIn2 fadeInBottom">PROGRAMACION EN PHP</h1>
+                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">APRENDERAS A USAR PHP, ADEMAS DE DIFERENTES FRAMEWORKS.</p>
                                               </div>
                                             </div>
                                           </div>
@@ -37,25 +37,25 @@
                                         <div class="page-header min-vh-75 m-3 border-radius-xl" style="background-image: url('https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/products/product-1-min.jpg');">
                                           <span class="mask bg-gradient-dark"></span>
                                           <div class="container">
-                                            <div class="row">
+                                            <div class="row justify-content-center">
                                               <div class="col-lg-6 my-auto">
-                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">Our Team</h4>
-                                                <h1 class="text-white fadeIn2 fadeInBottom">Work with the best</h1>
-                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">Free people make free choices. Free choices mean you get unequal outcomes. You can have freedom, or you can have equal outcomes. You can’t have both.</p>
+                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">CURSO</h4>
+                                                <h1 class="text-white fadeIn2 fadeInBottom">PROGRAMACION IoT CON PYTHON</h1>
+                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">APRENDERAS A USAR ARDUINO Y MANEJAR EL LENGUAJE PYTHON.</p>
                                               </div>
                                             </div>
                                           </div>
-                                        </div>
+                                        </div>  
                                       </div>
                                       <div class="carousel-item active">
                                         <div class="page-header min-vh-75 m-3 border-radius-xl" style="background-image: url('https://demos.creative-tim.com/test/material-dashboard-pro/assets/img/products/product-2-min.jpg');">
                                           <span class="mask bg-gradient-dark"></span>
                                           <div class="container">
-                                            <div class="row">
+                                            <div class="row justify-content-center">
                                               <div class="col-lg-6 my-auto">
-                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">Office Places</h4>
-                                                <h1 class="text-white fadeIn2 fadeInBottom">Work from home</h1>
-                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">You’re spending time to save money when you should be spending money to save time.</p>
+                                                <h4 class="text-white mb-0 fadeIn1 fadeInBottom">CURSO</h4>
+                                                <h1 class="text-white fadeIn2 fadeInBottom">FUNDAMENTO DE POO</h1>
+                                                <p class="lead text-white opacity-8 fadeIn3 fadeInBottom">APRENDERAS ACERCA DE LOS FUNDAMENTOS DE POO.</p>
                                               </div>
                                             </div>
                                           </div>
