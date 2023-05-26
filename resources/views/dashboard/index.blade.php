ec40db705510e0dd1fb4e06ad06c452b66f27409
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-25 19:05:46 -0600

    Modificaciones

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -4,29 +4,19 @@
         <!-- Navbar -->
         <x-navbars.navs.auth titlePage="Dashboard"></x-navbars.navs.auth>
         <!-- End Navbar -->
-
         @foreach($cursos as $curso)
             <h3>{{$curso->nombre}}</h3>
             <p>{{$curso->descripcion}}</p>
-           
         @foreach ($curso->semestres as $semestre)
             <h4>Semestre: {{ $semestre->nombre }}</h4>
-        
             <ul>
                 @foreach ($semestre->temas as $tema)
                     <li>{{ $tema->nombre }}</li>
                     
                 @endforeach
             </ul>
-
-            <ul>
-               
-                
-            </ul>
-
             @endforeach
         @endforeach
-        
     </main>
 </x-layout>
 
