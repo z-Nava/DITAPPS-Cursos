ecabcd41e4c7bd5d57bef44af4df1b6a79b285c4
z-Nava <62721391+z-Nava@users.noreply.github.com>
2023-05-27 12:13:47 -0600

    cambios

--- app/Http/Controllers/GestionActividadesController.php
+++ app/Http/Controllers/GestionActividadesController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class GestionActividadesController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('gestionActividades');
+    }
 }
